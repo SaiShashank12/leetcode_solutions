bc456726b38ad243931f915726dae67c7a7f5b3b
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2021-12-25 21:49:11 -0500

    Time: 33 ms (11.05%), Space: 14.3 MB (48.53%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -1,30 +1,47 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    
-    def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        def helper(root,result):
-            nonlocal sum1
-            if not root:
-                return 
-            result.append(root.val)
-            if root.left:
-                helper(root.left,result)
-            if root.right:
-                helper(root.right,result)
-            if not root.left and not root.right:
-                num=0
-                #result=result[::-1]
-                k=len(result)-1
-                for i in range(len(result)):
-                    num+=result[i]*(10**k)
-                    k-=1
-                sum1+=num
-            result.pop()
-        sum1=0
-        helper(root,[])
-        return sum1
+    def sumNumbers(self, root: TreeNode):
+        root_to_leaf = curr_number = 0
+        
+        while root:  
+            # If there is a left child,
+            # then compute the predecessor.
+            # If there is no link predecessor.right = root --> set it.
+            # If there is a link predecessor.right = root --> break it.
+            if root.left: 
+                # Predecessor node is one step to the left 
+                # and then to the right till you can.
+                predecessor = root.left 
+                steps = 1
+                while predecessor.right and predecessor.right is not root: 
+                    predecessor = predecessor.right 
+                    steps += 1
+
+                # Set link predecessor.right = root
+                # and go to explore the left subtree
+                if predecessor.right is None:
+                    curr_number = curr_number * 10 + root.val                    
+                    predecessor.right = root  
+                    root = root.left  
+                # Break the link predecessor.right = root
+                # Once the link is broken, 
+                # it's time to change subtree and go to the right
+                else:
+                    # If you're on the leaf, update the sum
+                    if predecessor.left is None:
+                        root_to_leaf += curr_number
+                    # This part of tree is explored, backtrack
+                    for _ in range(steps):
+                        curr_number //= 10
+                    predecessor.right = None
+                    root = root.right 
+                    
+            # If there is no left child
+            # then just go right.        
+            else: 
+                curr_number = curr_number * 10 + root.val
+                # if you're on the leaf, update the sum
+                if root.right is None:
+                    root_to_leaf += curr_number
+                root = root.right
+                        
+        return root_to_leaf
