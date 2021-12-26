aa15da56e18de0a228c227949d1397946d5b0731
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2021-12-25 21:50:12 -0500

    Time: 30 ms (60.04%), Space: 14.3 MB (48.53%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -1,47 +1,17 @@
 class Solution:
     def sumNumbers(self, root: TreeNode):
-        root_to_leaf = curr_number = 0
+        root_to_leaf = 0
+        stack = [(root, 0) ]
         
-        while root:  
-            # If there is a left child,
-            # then compute the predecessor.
-            # If there is no link predecessor.right = root --> set it.
-            # If there is a link predecessor.right = root --> break it.
-            if root.left: 
-                # Predecessor node is one step to the left 
-                # and then to the right till you can.
-                predecessor = root.left 
-                steps = 1
-                while predecessor.right and predecessor.right is not root: 
-                    predecessor = predecessor.right 
-                    steps += 1
-
-                # Set link predecessor.right = root
-                # and go to explore the left subtree
-                if predecessor.right is None:
-                    curr_number = curr_number * 10 + root.val                    
-                    predecessor.right = root  
-                    root = root.left  
-                # Break the link predecessor.right = root
-                # Once the link is broken, 
-                # it's time to change subtree and go to the right
-                else:
-                    # If you're on the leaf, update the sum
-                    if predecessor.left is None:
-                        root_to_leaf += curr_number
-                    # This part of tree is explored, backtrack
-                    for _ in range(steps):
-                        curr_number //= 10
-                    predecessor.right = None
-                    root = root.right 
-                    
-            # If there is no left child
-            # then just go right.        
-            else: 
+        while stack:
+            root, curr_number = stack.pop()
+            if root is not None:
                 curr_number = curr_number * 10 + root.val
-                # if you're on the leaf, update the sum
-                if root.right is None:
+                # if it's a leaf, update root-to-leaf sum
+                if root.left is None and root.right is None:
                     root_to_leaf += curr_number
-                root = root.right
+                else:
+                    stack.append((root.right, curr_number))
+                    stack.append((root.left, curr_number))
                         
         return root_to_leaf
