ab0c0295172c4fc682e746921de81eb6b1b23c96
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2021-12-28 15:34:54 -0500

    Time: 108 ms (28.89%), Space: 17.3 MB (28.72%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,5 +1,9 @@
-from collections import defaultdict, deque
+from collections import defaultdict
 class Solution:
+
+    WHITE = 1
+    GRAY = 2
+    BLACK = 3
 
     def findOrder(self, numCourses, prerequisites):
         """
@@ -8,34 +12,44 @@ class Solution:
         :rtype: List[int]
         """
 
-        # Prepare the graph
+        # Create the adjacency list representation of the graph
         adj_list = defaultdict(list)
-        indegree = {}
+
+        # A pair [a, b] in the input represents edge from b --> a
         for dest, src in prerequisites:
             adj_list[src].append(dest)
 
-            # Record each node's in-degree
-            indegree[dest] = indegree.get(dest, 0) + 1
-
-        # Queue for maintainig list of nodes that have 0 in-degree
-        zero_indegree_queue = deque([k for k in range(numCourses) if k not in indegree])
-
         topological_sorted_order = []
+        is_possible = True
 
-        # Until there are nodes in the Q
-        while zero_indegree_queue:
+        # By default all vertces are WHITE
+        color = {k: Solution.WHITE for k in range(numCourses)}
+        def dfs(node):
+            nonlocal is_possible
 
-            # Pop one node with 0 in-degree
-            vertex = zero_indegree_queue.popleft()
-            topological_sorted_order.append(vertex)
+            # Don't recurse further if we found a cycle already
+            if not is_possible:
+                return
 
-            # Reduce in-degree for all the neighbors
-            if vertex in adj_list:
-                for neighbor in adj_list[vertex]:
-                    indegree[neighbor] -= 1
+            # Start the recursion
+            color[node] = Solution.GRAY
 
-                    # Add neighbor to Q if in-degree becomes 0
-                    if indegree[neighbor] == 0:
-                        zero_indegree_queue.append(neighbor)
+            # Traverse on neighboring vertices
+            if node in adj_list:
+                for neighbor in adj_list[node]:
+                    if color[neighbor] == Solution.WHITE:
+                        dfs(neighbor)
+                    elif color[neighbor] == Solution.GRAY:
+                         # An edge to a GRAY vertex represents a cycle
+                        is_possible = False
 
-        return topological_sorted_order if len(topological_sorted_order) == numCourses else []
+            # Recursion ends. We mark it as black
+            color[node] = Solution.BLACK
+            topological_sorted_order.append(node)
+
+        for vertex in range(numCourses):
+            # If the node is unprocessed, then call dfs on it.
+            if color[vertex] == Solution.WHITE:
+                dfs(vertex)
+
+        return topological_sorted_order[::-1] if is_possible else []
