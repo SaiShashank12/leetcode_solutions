71ac2ae107351d0acb7beceaccb90c8618f30a7a
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2021-12-28 15:34:21 -0500

    Time: 100 ms (63.42%), Space: 15.7 MB (66.45%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,34 +1,41 @@
-from collections import defaultdict
-class node:
-    def __init__(self):
-        self.val=[]
-        self.count=0
+from collections import defaultdict, deque
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-#         if not prerequisites and numCourses>0:
-#             return [i for i in reversed(range(numCourses))]
-        
-        Graph=defaultdict(node)
-        for i in prerequisites:
-            #Graph[i[0]].val.append(i[1])
-            Graph[i[1]].val.append(i[0])
-            Graph[i[0]].count+=1
-        for i in range(numCourses):
-            if i not in Graph:
-                Graph[i]
-            
-        startnodes=[]
-        for k,v in Graph.items():
-            if not v.count:
-                startnodes.append(k)
-        result=[]
-        while startnodes:
-            tmp=startnodes.pop(0)
-            result.append(tmp)
-            for i in Graph[tmp].val:
-                Graph[i].count-=1
-                if Graph[i].count==0:
-                    startnodes.append(i)
-        return result if len(result)==numCourses else []
-                    
-                
+
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+
+        # Prepare the graph
+        adj_list = defaultdict(list)
+        indegree = {}
+        for dest, src in prerequisites:
+            adj_list[src].append(dest)
+
+            # Record each node's in-degree
+            indegree[dest] = indegree.get(dest, 0) + 1
+
+        # Queue for maintainig list of nodes that have 0 in-degree
+        zero_indegree_queue = deque([k for k in range(numCourses) if k not in indegree])
+
+        topological_sorted_order = []
+
+        # Until there are nodes in the Q
+        while zero_indegree_queue:
+
+            # Pop one node with 0 in-degree
+            vertex = zero_indegree_queue.popleft()
+            topological_sorted_order.append(vertex)
+
+            # Reduce in-degree for all the neighbors
+            if vertex in adj_list:
+                for neighbor in adj_list[vertex]:
+                    indegree[neighbor] -= 1
+
+                    # Add neighbor to Q if in-degree becomes 0
+                    if indegree[neighbor] == 0:
+                        zero_indegree_queue.append(neighbor)
+
+        return topological_sorted_order if len(topological_sorted_order) == numCourses else []
