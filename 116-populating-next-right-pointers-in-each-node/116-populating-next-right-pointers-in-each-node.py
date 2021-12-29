1621242739d61e12e856d7b314f399613b30f3fb
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2021-12-29 00:40:44 -0500

    Time: 99 ms (9.83%), Space: 15.8 MB (8.71%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -1,24 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+from collections import deque
 class Solution:
-    def connect(self, root: 'Node') -> 'Node':
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if not root:
             return root
-        next_level=[root]
-        result=[]
-        while(next_level):
-            result.append([i for i in next_level])
-            current_level=next_level
-            next_level=[]
-            while current_level:
-                tmp=current_level.pop(0)
+        Q=deque([root])
+        while Q:
+            size=len(Q)
+            for i in range(size):
+                tmp=Q.popleft()
+                if i<size-1:
+                    tmp.next=Q[0]
                 if tmp.left:
-                    next_level.append(tmp.left)
+                    Q.append(tmp.left)
                 if tmp.right:
-                    next_level.append(tmp.right)
-        for i in result:
-            curr=i[0]
-            for j in range(1,len(i)):
-                curr.next=i[j]
-                curr=curr.next
-            curr.next=None
-            
+                    Q.append(tmp.right)
         return root
