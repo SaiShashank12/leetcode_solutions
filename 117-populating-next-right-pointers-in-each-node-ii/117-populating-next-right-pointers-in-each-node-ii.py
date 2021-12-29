77be41d967559e84277a085e5e2abfd20e31de02
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2021-12-29 00:22:19 -0500

    Time: 89 ms (5.02%), Space: 15.3 MB (96.03%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
@@ -1,34 +1,22 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
-
+import collections
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
         if not root:
             return root
-        next_level=[root]
-        result=[]
-        while(next_level):
-            result.append([i for i in next_level])
-            current_level=next_level
-            next_level=[]
-            while current_level:
-                tmp=current_level.pop(0)
-                if tmp.left:
-                    next_level.append(tmp.left)
-                if tmp.right:
-                    next_level.append(tmp.right)
-        for i in result:
-            curr=i[0]
-            for j in range(1,len(i)):
-                curr.next=i[j]
-                curr=curr.next
-            curr.next=None
-            
+        
+        Q=collections.deque([root])
+        
+        while Q:
+            size=len(Q)
+            for i in range(size):
+                node=Q.popleft()
+
+                if i<size-1:
+                    node.next=Q[0]
+
+                if node.left:
+                    Q.append(node.left)
+                if node.right:
+                    Q.append(node.right)
+                
         return root
