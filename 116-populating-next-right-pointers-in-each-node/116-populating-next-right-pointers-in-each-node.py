7235a6f209334662c054cedab427c0980535bfc9
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2021-12-29 00:42:39 -0500

    Time: 102 ms (8.96%), Space: 15.6 MB (70.69%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -1,26 +1,33 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
-from collections import deque
 class Solution:
-    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+    def connect(self, root: 'Node') -> 'Node':
+        
         if not root:
             return root
-        Q=deque([root])
-        while Q:
-            size=len(Q)
-            for i in range(size):
-                tmp=Q.popleft()
-                if i<size-1:
-                    tmp.next=Q[0]
-                if tmp.left:
-                    Q.append(tmp.left)
-                if tmp.right:
-                    Q.append(tmp.right)
-        return root
+        
+        # Start with the root node. There are no next pointers
+        # that need to be set up on the first level
+        leftmost = root
+        
+        # Once we reach the final level, we are done
+        while leftmost.left:
+            
+            # Iterate the "linked list" starting from the head
+            # node and using the next pointers, establish the 
+            # corresponding links for the next level
+            head = leftmost
+            while head:
+                
+                # CONNECTION 1
+                head.left.next = head.right
+                
+                # CONNECTION 2
+                if head.next:
+                    head.right.next = head.next.left
+                
+                # Progress along the list (nodes on the current level)
+                head = head.next
+            
+            # Move onto the next level
+            leftmost = leftmost.left
+        
+        return root 
