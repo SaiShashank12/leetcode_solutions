a8e5c6d48cbbf3baff6a8fec0865a3a82ad9cec5
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2021-12-30 17:40:19 -0500

    Time: 170 ms (6.64%), Space: 15.4 MB (88.40%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.py
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -1,36 +1,28 @@
 from collections import defaultdict
 
 class Solution:
-    def __init__(self):
-        self.seen=[]
-    def cycle (self,Graph,curr):
-        self.seen.append(curr)
-        if not Graph[curr]:
-            return
-        while Graph[curr]:
-            #Graph[curr].remove(curr)
-            tmp=Graph[curr].pop()
-            Graph[tmp].remove(curr)
-            self.cycle(Graph,tmp)
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if n==1:
-            return True
-        Graph=defaultdict(list)
-         
-        
-        #node=set()
-        for i in edges:
-            
-            #node.update(i)
-            Graph[i[0]].append(i[1])
-            
-            Graph[i[1]].append(i[0])
-        if len(Graph)<n:
-            return False
-        
-        self.cycle(Graph,list(Graph.keys())[0])
-        if len(self.seen)==n:
-            return True
-        return False
+    
+        if len(edges) != n - 1: return False
+
+        adj_list = [[] for _ in range(n)]
+        for A, B in edges:
+            adj_list[A].append(B)
+            adj_list[B].append(A)
+
+        parent = {0: -1}
+        queue = collections.deque([0])
+
+        while queue:
+            node = queue.popleft()
+            for neighbour in adj_list[node]:
+                if neighbour == parent[node]:
+                    continue
+                if neighbour in parent:
+                    return False
+                parent[neighbour] = node
+                queue.append(neighbour)
+
+        return len(parent) == n
             
             
