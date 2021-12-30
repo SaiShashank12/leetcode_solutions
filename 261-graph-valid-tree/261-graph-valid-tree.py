b9aa5bc2aafc81eed39ad0057a4d485ce98e3fc0
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2021-12-30 17:41:21 -0500

    Time: 100 ms (31.29%), Space: 17.8 MB (9.30%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.py
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -1,29 +1,36 @@
 from collections import defaultdict
 
 class Solution:
+    def __init__(self):
+        self.seen=[]
+    def cycle (self,Graph,curr):
+        self.seen.append(curr)
+        if not Graph[curr]:
+            return
+        while Graph[curr]:
+            #Graph[curr].remove(curr)
+            tmp=Graph[curr].pop()
+            Graph[tmp].remove(curr)
+            self.cycle(Graph,tmp)
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-    
-        if len(edges) != n - 1: return False
-
-        # Create an adjacency list.
-        adj_list = [[] for _ in range(n)]
-        for A, B in edges:
-            adj_list[A].append(B)
-            adj_list[B].append(A)
-
-        # We still need a seen set to prevent our code from infinite
-        # looping if there *is* cycles (and on the trivial cycles!)
-        seen = {0}
-        stack = [0]
-
-        while stack:
-            node = stack.pop()
-            for neighbour in adj_list[node]:
-                if neighbour in seen:
-                    continue
-                seen.add(neighbour)
-                stack.append(neighbour)
-
-        return len(seen) == n
+        if n==1:
+            return True
+        Graph=defaultdict(list)
+         
+        
+        #node=set()
+        for i in edges:
+            
+            #node.update(i)
+            Graph[i[0]].append(i[1])
+            
+            Graph[i[1]].append(i[0])
+        if len(Graph)<n:
+            return False
+        
+        self.cycle(Graph,list(Graph.keys())[0])
+        if len(self.seen)==n:
+            return True
+        return False
             
             
