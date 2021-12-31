9add310c914b19b8d28ae391ab9d65f49c22884c
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2021-12-30 20:20:59 -0500

    Time: 1760 ms (84.31%), Space: 15.2 MB (6.19%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.py
+++ 277-find-the-celebrity/277-find-the-celebrity.py
@@ -1,13 +1,16 @@
 # The knows API is already defined for you.
 # return a bool, whether a knows b
 # def knows(a: int, b: int) -> bool:
-
+from functools import lru_cache
 class Solution:
+    @lru_cache(maxsize=None)
+    def cachedKnows(self,a,b):
+        return knows(a,b)
     def findCelebrity(self, n: int) -> int:
         self.n=n
         candidate_celb=0
         for i in range(1,n):
-            if knows(candidate_celb,i):
+            if self.cachedKnows(candidate_celb,i):
                 candidate_celb=i
         if self.is_know(candidate_celb):
             return candidate_celb
@@ -15,6 +18,6 @@ class Solution:
     def is_know(self,i):
         for j in range(self.n):
             if i==j:continue
-            if knows(i,j) or not knows(j,i):
+            if self.cachedKnows(i,j) or not self.cachedKnows(j,i):
                 return False
         return True
