1de44d5d35dbcf226561190a9314ad04959d2362
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2021-12-30 19:41:28 -0500

    Time: 79 ms (31.09%), Space: 14.2 MB (85.71%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
@@ -1,11 +1,8 @@
 class Solution:
-    def smallestRepunitDivByK(self, k: int) -> int:
-        if k%2==0 or k==0 or k%5==0:
-            return -1
-        remainder=0
-        for i in range(1,k+1):
-            remainder=(remainder*10+1)%k
-            if remainder==0:
-                return i
+    def smallestRepunitDivByK(self, K: int) -> int:
+        remainder = 0
+        for length_N in range(1,K+1):
+            remainder = (remainder*10+1) % K
+            if remainder == 0:
+                return length_N
         return -1
-        
