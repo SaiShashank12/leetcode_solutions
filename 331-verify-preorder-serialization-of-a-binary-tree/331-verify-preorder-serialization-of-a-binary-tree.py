cb3b8098b02ddebb7c77d1077e699455eee72c36
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-01-02 22:37:59 -0500

    Time: 36 ms (60.68%), Space: 14.3 MB (67.86%) - LeetHub

--- 331-verify-preorder-serialization-of-a-binary-tree/331-verify-preorder-serialization-of-a-binary-tree.py
+++ 331-verify-preorder-serialization-of-a-binary-tree/331-verify-preorder-serialization-of-a-binary-tree.py
@@ -2,18 +2,23 @@ class Solution:
     def isValidSerialization(self, preorder: str) -> bool:
         # number of available slots
         slots = 1
-
-        for node in preorder.split(','):
-            # one node takes one slot
-            slots -= 1
-            
-            # no more slots available
-            if slots < 0:
-                return False
-            
-            # non-empty node creates two children slots
-            if node != '#':
-                slots += 2
         
+        prev = None  # previous character
+        for ch in preorder:
+            if ch == ',':
+                # one node takes one slot
+                slots -= 1
+
+                # no more slots available
+                if slots < 0:
+                    return False
+
+                # non-empty node creates two children slots
+                if prev != '#':
+                    slots += 2
+            prev = ch
+        
+        # the last node
+        slots = slots + 1 if ch != '#' else slots - 1 
         # all slots should be used up
         return slots == 0
