1a5d8970a7ff34ccc4af3aebfd8299a16753d77a
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-01-02 20:28:00 -0500

    Time: 948 ms (14.22%), Space: 18.8 MB (97.64%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.py
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -3,14 +3,15 @@ class Solution:
 
         if len(trust) < N - 1:
             return -1
-
-        trust_scores = [0] * (N + 1)
+    
+        indegree = [0] * (N + 1)
+        outdegree = [0] * (N + 1)
 
         for a, b in trust:
-            trust_scores[a] -= 1
-            trust_scores[b] += 1
+            outdegree[a] += 1
+            indegree[b] += 1
 
-        for i, score in enumerate(trust_scores[1:], 1):
-            if score == N - 1:
+        for i in range(1, N + 1):
+            if indegree[i] == N - 1 and outdegree[i] == 0:
                 return i
         return -1
