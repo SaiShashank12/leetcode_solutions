07daba8998e53bac8c7e27dc1e7c545de1fa338c
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-01-04 13:02:09 -0500

    Time: 32 ms (53.94%), Space: 14 MB (89.54%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
@@ -1,21 +1,12 @@
 class Solution:
-    def bitwiseComplement(self, n: int) -> int:
-        dir1=str(bin(n)[2:])
-        result=[]
-        for i in dir1:
-            x='0' if i=='1' else '1'
-            result.append(x)
-        def binaryToDecimal(binary):
-            binary1 = binary
-            decimal, i, n = 0, 0, 0
-            while(binary != 0):
-                dec = binary % 10
-                decimal = decimal + dec * pow(2, i)
-                binary = binary//10
-                i += 1
-            return decimal 
-        str1="".join(result)
-        print(str1)
-        return binaryToDecimal(int(str1))
-        #print(result)
-        
+    def bitwiseComplement(self, N: int) -> int:
+        if N == 0:
+            return 1
+        todo, bit = N, 1
+        while todo:
+            # flip current bit
+            N = N ^ bit
+            # prepare for the next run
+            bit = bit << 1
+            todo = todo >> 1
+        return N
