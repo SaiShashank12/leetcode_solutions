c610313ec0eb3e70f820d6c56b1b30ddc6d7c28c
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-01-06 13:54:16 -0500

    Time: 68 ms (69.03%), Space: 14.8 MB (77.09%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,14 +1,16 @@
 from collections import defaultdict
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        timestamp=defaultdict(int)
+        start=defaultdict(int)
+        end=defaultdict(int)
         for i in trips:
-            timestamp[i[1]]-=i[0]
-            timestamp[i[2]]+=i[0]
-        # min_start=min(timestamp.keys())
-        # max_end=max(timestamp.keys())
-        for i in sorted(timestamp.keys()):
-            capacity+=timestamp[i]
+            start[i[1]]+=i[0]
+            end[i[2]]+=i[0]
+        min_start=min(start.keys())
+        max_end=max(end.keys())
+        for i in range(min_start,max_end):
+            capacity+=end[i]
+            capacity-=start[i]
             if capacity<0:
                 return False
         return True
