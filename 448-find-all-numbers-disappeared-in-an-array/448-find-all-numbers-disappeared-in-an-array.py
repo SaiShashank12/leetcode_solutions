a1cd6e64705cfd1e1a1fd6eeda5235e88b192294
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-01-09 14:27:09 -0500

    Time: 713 ms (5.02%), Space: 22 MB (74.10%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -1,9 +1,30 @@
-from collections import Counter
-class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
         
-        unique=set(nums)
-        n=len(nums)
-        arr=[i for i in range(1,n+1)]
+        # Iterate over each of the elements in the original array
+        for i in range(len(nums)):
+            
+            # Treat the value as the new index
+            new_index = abs(nums[i]) - 1
+            
+            # Check the magnitude of value at this new index
+            # If the magnitude is positive, make it negative 
+            # thus indicating that the number nums[i] has 
+            # appeared or has been visited.
+            if nums[new_index] > 0:
+                nums[new_index] *= -1
         
-        return [item for item in arr if item not in unique]
+        # Response array that would contain the missing numbers
+        result = []    
+        
+        # Iterate over the numbers from 1 to N and add all those
+        # that have positive magnitude in the array 
+        for i in range(1, len(nums) + 1):
+            if nums[i - 1] > 0:
+                result.append(i)
+                
+        return result        
