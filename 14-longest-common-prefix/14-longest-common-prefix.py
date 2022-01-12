aaede97ca864c092a27edd04cc9257f40a083988
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-01-11 21:11:47 -0500

    Time: 40 ms (40.92%), Space: 14.2 MB (94.37%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,15 +1,35 @@
+index=""
+class Trienode:
+    def __init__(self):
+        
+        self.children={}
+        self.isLeaf=False
 class Solution:
+    def __init__(self):
+        self.root=Trienode()
+    def contructTrie(self,strs):
+        for i in strs:
+            tmp_root=self.root
+            for j in i:
+                if j not in tmp_root.children:
+                    tmp_root.children[j]=Trienode()
+                tmp_root=tmp_root.children[j]
+            tmp_root.isLeaf=True
+    def count_children(self,node):
+        count=0
+        for i in node.children.keys():
+            count+=1
+            global index
+            index=i
+        return count
+    def walk_trie(self):
+        pcrawl=self.root
+        prefix=""
+        while(self.count_children(pcrawl)==1 and pcrawl.isLeaf==False):
+            pcrawl=pcrawl.children[index]
+            prefix+=index
+        return prefix
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        i = 0
-        confirmed, possible = '', ''
-        while i < len(max(strs)):
-            try:
-                possible = strs[0][i]
-                for x in strs:
-                    if possible != x[i]:
-                        return confirmed
-                confirmed += strs[0][i]
-                i += 1
-            except:
-                return confirmed
-        return confirmed    
+        self.contructTrie(strs)   
+        return self.walk_trie()
+        
