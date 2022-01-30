4c53eecc64ed3f556be87331bf94f9dc04ac19d6
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-01-30 13:32:48 -0500

    Time: 2136 ms (18.51%), Space: 25.4 MB (83.32%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.py
@@ -0,0 +1,11 @@
+from collections import deque
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        while k:
+            nums.insert(0,nums.pop())
+            k-=1
+            
+            
