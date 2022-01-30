eea83957d7505c0405914e6672c793ee289d0432
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-01-30 13:33:36 -0500

    Time: 216 ms (85.71%), Space: 25.4 MB (83.32%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,11 +1,13 @@
-from collections import deque
 class Solution:
+    def reverse(self, nums: list, start: int, end: int) -> None:
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start, end = start + 1, end - 1
+                
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        while k:
-            nums.insert(0,nums.pop())
-            k-=1
-            
-            
+        n = len(nums)
+        k %= n
+
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
