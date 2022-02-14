5aa5f6813731287f7f47d4ea3d6bf55cf593bf38
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-02-14 11:07:20 -0500

    Time: 1156 ms (39.22%), Space: 17.1 MB (99.24%) - LeetHub

--- 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
@@ -3,17 +3,13 @@ class Solution:
         ones = sum(data)
         cnt_one = max_one = 0
 
-        # maintain a deque with the size = ones
-        deque = collections.deque()
-        for i in range(len(data)):
-
-            # we would always add the new element into the deque
-            deque.append(data[i])
-            cnt_one += data[i]
-
-            # when there are more than ones elements in the deque,
-            # remove the leftmost one
-            if len(deque) > ones:
-                cnt_one -= deque.popleft()
-            max_one = max(max_one, cnt_one)
-        return ones - max_one
+        right=left=0
+        while right<len(data):
+            cnt_one+=data[right]
+            right+=1
+            
+            if right-left>ones:
+                cnt_one-=data[left]
+                left+=1
+            max_one=max(max_one,cnt_one)
+        return ones-max_one
