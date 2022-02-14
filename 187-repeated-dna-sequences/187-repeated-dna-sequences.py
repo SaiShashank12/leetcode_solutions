625d52cd74757717e4d70d5adfc5d57000055c61
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-02-14 11:32:58 -0500

    Time: 143 ms (14.69%), Space: 26 MB (85.71%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -1,14 +1,15 @@
-from collections import defaultdict
+
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        hash_map=defaultdict(int)
+        hash_map=set()
         right=10
         left=0
         result=set()
         while right<=len(s):
-            hash_map[s[left:right]]+=1
-            if hash_map[s[left:right]]>1:
+            if s[left:right] in hash_map:
                 result.add(s[left:right])
+            hash_map.add(s[left:right])
+            
             left+=1
             right+=1
         return list(result)
