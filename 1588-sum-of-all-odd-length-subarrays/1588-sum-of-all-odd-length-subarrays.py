3cdc775d557528ee5039aa12468bd51867bd493e
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-02-24 19:57:06 -0500

    Time: 72 ms (60.46%), Space: 13.9 MB (61.53%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
@@ -1,9 +1,7 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        # corner case
-        
-        res = 0; freq = 0; n = len(arr)
-        for i in range(n):
-            freq = freq-(i+1)//2+(n-i+1)//2
-            res += freq*arr[i]
-        return res
+        s=0
+        for i in range(len(arr)):
+            for j in range(i,len(arr),2):
+                s+=sum(arr[i:j+1])
+        return s
