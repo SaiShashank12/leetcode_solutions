e977f7a738344a1b74c1ac3c7efaa628db41a91f
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-02-24 19:56:14 -0500

    Time: 67 ms (67.77%), Space: 13.9 MB (61.53%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
@@ -1,9 +1,9 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        # Brute Force O(n^3)
-        # Alternative O(n). Given the length n of arr, how many times will arr[k] appear in the total sum?
+        # corner case
         
-        res = 0
-        for i in range(len(arr)):
-            res += ((i + 1) * (len(arr) - i) + 1) // 2 * arr[i]
+        res = 0; freq = 0; n = len(arr)
+        for i in range(n):
+            freq = freq-(i+1)//2+(n-i+1)//2
+            res += freq*arr[i]
         return res
