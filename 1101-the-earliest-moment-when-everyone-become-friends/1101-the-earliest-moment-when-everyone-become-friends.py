d8984c8bdbcfd0076403bcffef5f83912fec7a33
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-02-27 15:28:48 -0500

    Time: 104 ms (98.02%), Space: 14.3 MB (80.94%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
@@ -1,56 +1,30 @@
 class Solution:
     def earliestAcq(self, logs: List[List[int]], n: int) -> int:
-        # First, we need to sort the events in chronological order.
-        logs.sort(key = lambda x: x[0])
-
-        uf = UnionFind(n)
-        # Initially, we treat each individual as a separate group.
-        group_cnt = n
-
-        # We merge the groups along the way.
-        for timestamp, friend_a, friend_b in logs:
-            if uf.union(friend_a, friend_b):
-                group_cnt -= 1
-
-            # The moment when all individuals are connected to each other.
-            if group_cnt == 1:
-                return timestamp
-
-        # There are still more than one groups left,
-        #  i.e. not everyone is connected.
-        return -1
-
-
-class UnionFind:
-
-    def __init__(self, size):
-        self.group = [group_id for group_id in range(size)]
-        self.rank = [0] * size
-
-    def find(self, person):
-        if self.group[person] != person:
-            self.group[person] = self.find(self.group[person])
-        return self.group[person]
-
-    def union(self, a, b):
-        """
-            return: true if a and b are not connected before
-                otherwise, connect a with b and then return false
-        """
-        group_a = self.find(a)
-        group_b = self.find(b)
-        is_merged = False
-        if group_a == group_b:
-            return is_merged
-
-        is_merged = True
-        # Merge the lower-rank group into the higher-rank group.
-        if self.rank[group_a] > self.rank[group_b]:
-            self.group[group_b] = group_a
-        elif self.rank[group_a] < self.rank[group_b]:
-            self.group[group_a] = group_b
-        else:
-            self.group[group_a] = group_b
-            self.rank[group_b] += 1
-
-        return is_merged
+        logs.sort(key=lambda x:x[0])
+        
+        
+        def Union(parent,x,y):
+            if parent[x]<=parent[y]:
+                parent[x]+=parent[y]
+                parent[y]=x
+            else:
+                parent[y]+=parent[x]
+                parent[x]=y
+        def find(parent,x):
+            if parent[x]<0:
+                return x
+            return find(parent,parent[x])
+        
+        def union_find(logs,n):
+            parent=[-1]*n
+            
+            for i in logs:
+                x=find(parent,i[1])
+                y=find(parent,i[2])
+                if x!=y:
+                    Union(parent,x,y)
+                    n-=1
+                    if n==1:
+                        return i[0]
+        time=union_find(logs,n)
+        return time if time else -1
