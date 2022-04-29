066b4266e3ff8012271dbbfe61cc61dd0e6aa6d0
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-04-29 13:44:11 -0400

    Time: 42 ms (74.79%), Space: 14.1 MB (14.59%) - LeetHub

--- 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.py
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.py
@@ -1,8 +1,36 @@
 class Solution:
-    def isMajorityElement(self, nums, target):
-        k = len(nums) // 2
-        if nums[k] != target:
-            return False
-        lo = bisect.bisect_left(nums, target, 0, k)
-        hi = lo + k
-        return hi < len(nums) and nums[hi] == target
+    def isMajorityElement(self, nums: List[int], target: int) -> bool:
+        def binary_search(nums,target):
+            left, right=0,len(nums)-1
+            last=len(nums)
+            while left<=right:
+                mid=(left+right)//2
+                
+                if nums[mid]==target and last>mid:
+                    last=mid
+                    
+                if nums[mid]<target:
+                    left=mid+1
+                else:
+                    right=mid-1
+                    
+            return last
+        def binary_search_(nums,target):
+            left, right=0,len(nums)-1
+            last=0
+            while left<=right:
+                mid=(left+right)//2
+                
+                if nums[mid]==target and last<mid:
+                    last=mid
+                    
+                if nums[mid]<=target:
+                    left=mid+1
+                else:
+                    right=mid-1
+                    
+            return last
+         
+    
+         
+        return len(nums)//2<(binary_search_(nums,target)-binary_search(nums,target))+1
