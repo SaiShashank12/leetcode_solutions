1155ed7c7b715cc83e7a8d8f1c53d224fc8f8378
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-05-15 21:54:44 -0400

    Time: 2211 ms (48.11%), Space: 0B (100.00%) - LeetHub

--- 1549-the-most-recent-orders-for-each-product/1549-the-most-recent-orders-for-each-product.sql
+++ 1549-the-most-recent-orders-for-each-product/1549-the-most-recent-orders-for-each-product.sql
@@ -1,7 +1,9 @@
-select b.product_name, a.product_id, a.order_id, a.order_date from Orders a
-join Products b
-on a.product_id = b.product_id
-where (a.product_id, a.order_date) in (
-select product_id, max(order_date) over (partition by product_id) as order_date
-from Orders)
-order by b.product_name, a.product_id, a.order_id
+SELECT product_name, product_id, order_id, order_date
+FROM (
+    SELECT product_name, P.product_id, order_id, order_date, RANK() OVER (PARTITION BY product_name ORDER BY order_date DESC) rnk
+    FROM Orders O
+    JOIN Products P
+    On O.product_id = P.product_id
+) temp
+WHERE rnk = 1
+ORDER BY product_name, product_id, order_id
