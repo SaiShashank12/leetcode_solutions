964286a05e1ef2ecf53504d0ad599875c7271a35
Sai Shashank Mudliar <34842779+SaiShashank12@users.noreply.github.com>
2022-05-15 21:28:22 -0400

    Time: 2302 ms (39.97%), Space: 0B (100.00%) - LeetHub

--- 1549-the-most-recent-orders-for-each-product/1549-the-most-recent-orders-for-each-product.sql
+++ 1549-the-most-recent-orders-for-each-product/1549-the-most-recent-orders-for-each-product.sql
@@ -2,7 +2,6 @@ select b.product_name, a.product_id, a.order_id, a.order_date from Orders a
 join Products b
 on a.product_id = b.product_id
 where (a.product_id, a.order_date) in (
-select product_id, max(order_date) as order_date
-from Orders
-group by product_id)
+select product_id, max(order_date) over (partition by product_id) as order_date
+from Orders)
 order by b.product_name, a.product_id, a.order_id
